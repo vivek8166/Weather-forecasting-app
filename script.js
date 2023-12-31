e7133d49f0bc0115a271c51f7c3f7d420bfcc835
vivek8166 <93274275+vivek8166@users.noreply.github.com>
2023-12-31 17:37:58 +0530

    first commit

--- script.js
+++ script.js
@@ -37,22 +37,22 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/images/cloud.png";
+            weather_img.src = "./images/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/images/clear.png";
+            weather_img.src = "./images/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/images/rain.png";
+            weather_img.src = "./images/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "/images/mist.png";
+            weather_img.src = "./images/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/images/snow.png";
+            weather_img.src = "./images/snow.png";
             break;
         case 'wind':
-            weather_img.src="/images/wind.png";
+            weather_img.src="./images/wind.png";
             break;
         
 
